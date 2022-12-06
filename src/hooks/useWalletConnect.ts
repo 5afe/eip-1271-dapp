b084049f191a9f2ddc69bb6e3b70264fbaac8b95
iamacook <aaron@safe.global>
2022-12-06 11:11:44 +0100

    fix: don't cache sessions

--- src/hooks/useWalletConnect.ts
+++ src/hooks/useWalletConnect.ts
@@ -4,11 +4,18 @@ import { useMemo } from 'react';
 
 import { WC_BRIDGE } from '@/config/constants';
 
+const STORAGE_KEY = 'walletconnect';
+
 const useWalletConnect = (): InstanceType<typeof WalletConnect> => {
+  if (localStorage.getItem(STORAGE_KEY)) {
+    localStorage.removeItem(STORAGE_KEY);
+  }
+
   return useMemo(() => {
     return new WalletConnect({
       bridge: WC_BRIDGE,
       qrcodeModal: QRCodeModal,
+      storageId: STORAGE_KEY,
     });
   }, []);
 };
